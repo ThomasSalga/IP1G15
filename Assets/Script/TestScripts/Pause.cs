c7f126f5d699e83bebd5b0c67f4fffa409748bd6
killianm97 <killianmangan@gmail.com>
2017-03-03 02:18:22 +0000

    I added some Prefabs without animations and tried make a pause menu
    Can you try get the pause implemented? I know it pauses the game when i
    press escape but it instantly unpauses the game too

---
+++ Assets/Script/TestScripts/Pause.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour 
+{
+	//[SerializeField] private
+	public GameObject pausePanel;
+	void Start()
+	{
+		pausePanel.SetActive(false);
+	}
+	void Update()
+	{
+		if (!pausePanel.activeInHierarchy) {
+			
+			if (Input.GetKeyDown (KeyCode.Escape)) {
+			
+		
+				if (!pausePanel.activeInHierarchy) {
+					//Debug.Log ("Game Paused");
+					StartCoroutine (PauseGame ());
+					//PauseGame ();
+
+				}
+
+			}
+
+
+
+		}
+
+
+		if (pausePanel.activeInHierarchy) {
+
+			if (Input.GetKeyDown (KeyCode.Escape)) {
+
+		
+			
+				if (pausePanel.activeInHierarchy) {
+					//Debug.Log ("Game Resumed");
+					StartCoroutine (ContinueGame ());   
+
+				}
+			} 
+		}
+	}
+
+	//private void PauseGame()
+	IEnumerator PauseGame()
+	{
+		Time.timeScale = 0;
+		pausePanel.SetActive(true);
+		Debug.Log ("Game Paused");
+		//Disable scripts that still work while timescale is set to 0
+		yield return new WaitForSecondsRealtime(2);
+	} 
+	IEnumerator ContinueGame()
+	{
+		Time.timeScale = 1;
+		pausePanel.SetActive(false);
+		Debug.Log ("Game Resumed");
+		yield return new WaitForSecondsRealtime(2);
+		//enable the scripts again
+	}
+}
